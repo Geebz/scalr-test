0ccf4a41627d6d39cdf185325c46657e7f09a62a
Geebz <alexandrveklich@gmail.com>
2023-09-26 15:47:16 +0300

    Update main.tf
    add aws provide settings

--- main.tf
+++ main.tf
@@ -15,3 +15,15 @@ resource "random_integer" "priority" {
   min = 2
   max = 500
 }
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "veklych-first-tf-bucket"
+
+  tags = {
+    Name        = "veklych-demo"
+  }
+}
